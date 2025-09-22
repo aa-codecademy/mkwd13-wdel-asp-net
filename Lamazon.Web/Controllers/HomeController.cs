05b197176025a28518920f812084f63f99c61b6a
toiise <toiise@hotmail.com>
2025-09-22 20:39:18 +0200

    Added several partial views, change in HomeController and added ProductsController

--- Lamazon.Web/Controllers/HomeController.cs
+++ Lamazon.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Lamazon.Services.Interfaces;
 using Lamazon.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,16 +7,17 @@ namespace Lamazon.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IProductService productService)
         {
-            _logger = logger;
+            _productService = productService;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var featuredProducts = _productService.GetAllFeaturedProducts();
+            return View(featuredProducts);
         }
 
         public IActionResult Privacy()
