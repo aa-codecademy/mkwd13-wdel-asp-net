d83725a0e9d853885a43c3449f0849512f3439f6
toiise <toiise@hotmail.com>
2025-10-03 20:36:45 +0200

    Added ProductCategoriesController and views

--- Lamazon.Services/Extensions/InjectionExtensions.cs
+++ Lamazon.Services/Extensions/InjectionExtensions.cs
@@ -31,6 +31,7 @@ namespace Lamazon.Services.Extensions
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IOrderService, OrderService>();
             services.AddScoped<IDashboardService , DashboardService>();
+            services.AddScoped<IProductCategoriesService, ProductCategoriesService>();
         }
 
         public static void InjectAutoMapper(this IServiceCollection services)
